8e09f4fed02e3f2f7bd3ee0d93a95dd0bdfb9b44
saurabhra <raisaurabh1992@gmail.com>
2018-08-29 23:50:17 +0530

    removing eclipse dot file and project file.

--- src/JavaBasicGeeksforGeeks/AsciiValue.java
+++ src/JavaBasicGeeksforGeeks/AsciiValue.java
@@ -3,13 +3,15 @@ package JavaBasicGeeksforGeeks;
 // https://www.geeksforgeeks.org/program-print-ascii-value-character/
 
 public class AsciiValue {
-public static void main(String[] args) {
-	char c = 'e';
-	int d=c;
-	System.out.println(d);
-	
-	System.out.println("Branch change");
 
+
+public static void main(String[] args) {
+	            char c = 'e';
+	int d=c;
+	            System.out.println(d);
+
+	System.out.println("Branch change");
+	System.out.println("Branch change");
 	System.out.println("new commit required");
 }
 }
